2bca85a67402479891a13e44d21131710e0ea8bb
Denis Li <denisli268@gmail.com>
2022-03-13 00:35:06 -0800

    Files for locally testing changes to inline diff custom matcher for snapshot-diff

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,89 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+// @ts-ignore
+import { snapshotDiff } from 'snapshot-diff';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders inline difference with snapshotDiff and toMatchInlineSnapshot', () => {
+  expect(snapshotDiff(<App />, <App flag={true} />)).toMatchInlineSnapshot(`
+"Snapshot Diff:
+- <App />
++ <App flag={true} />
+
+@@ -8,15 +8,11 @@
+        alt=\\"logo\\"
+        className=\\"App-logo\\"
+        src=\\"logo.svg\\"
+      />
+      <p>
+-       Edit 
+-       <code>
+-         src/App.tsx
+-       </code>
+-        and save to reload.
++       Flag!
+      </p>
+      <a
+        className=\\"App-link\\"
+        href=\\"https://reactjs.org\\"
+        rel=\\"noopener noreferrer\\""
+`);
+ });
+
+test('renders inline difference with toMatchInlineDiffSnapshot', () => {
+  expect(<App />).toMatchInlineDiffSnapshot(<App flag={true} />, `
+"Snapshot Diff:
+- <App />
++ <App flag={true} />
+
+@@ -8,15 +8,11 @@
+        alt=\\"logo\\"
+        className=\\"App-logo\\"
+        src=\\"logo.svg\\"
+      />
+      <p>
+-       Edit 
+-       <code>
+-         src/App.tsx
+-       </code>
+-        and save to reload.
++       Flag!
+      </p>
+      <a
+        className=\\"App-link\\"
+        href=\\"https://reactjs.org\\"
+        rel=\\"noopener noreferrer\\""
+`)
+});
+
+test('renders inline difference with toMatchInlineDiffSnapshot with options', () => {
+  expect(<App />).toMatchInlineDiffSnapshot(<App flag={true} />, { stablePatchmarks: true }, `
+"Snapshot Diff:
+- <App />
++ <App flag={true} />
+
+@@ --- --- @@
+        alt=\\"logo\\"
+        className=\\"App-logo\\"
+        src=\\"logo.svg\\"
+      />
+      <p>
+-       Edit 
+-       <code>
+-         src/App.tsx
+-       </code>
+-        and save to reload.
++       Flag!
+      </p>
+      <a
+        className=\\"App-link\\"
+        href=\\"https://reactjs.org\\"
+        rel=\\"noopener noreferrer\\""
+`);
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import 'snapshot-diff/extend-expect';
+
+import { toBeArray, toBeSealed } from 'jest-extended';
+expect.extend({ toBeArray, toBeSealed });
